15d323bf412e9339aadc26fe02051aa80efc88d8
John | Elite Encoder <john@eliteencoder.net>
2025-07-02 20:56:57 +0000

    fix segfault in comfystream worker

--- webrtc-stream/worker/main.py
+++ webrtc-stream/worker/main.py
@@ -6,7 +6,12 @@ import sys
 import os
 # import av
 # import numpy as np
-# import torch
+import torch
+# Initialize CUDA before any other imports to prevent core dump.
+if torch.cuda.is_available():
+    torch.cuda.init()
+
+
 from fastapi import FastAPI, Request, HTTPException, Body
 from fastapi.responses import PlainTextResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -139,22 +144,17 @@ class WebRTCServer:
     async def init_pipeline(self):
         """Initialize the ComfyStream pipeline"""
         
-        kwargs = {
-            "cwd": "/workspace/ComfyUI",
-            "disable_cuda_malloc": True,
-            "gpu_only": True,
-            "preview_method": "none"
-        }
-        
         try:
             logger.info("Initializing ComfyStream pipeline...")
             self.pipeline = Pipeline(
                 width=512,
                 height=512,
-                comfyui_inference_log_level=logging.INFO,
-                **kwargs
+                comfyui_inference_log_level="DEBUG",
+                cwd="/workspace/ComfyUI",
+                disable_cuda_malloc=True, 
+                gpu_only=True,
+                preview_method="none"
             )
-            
             
             logging.info(f"Pipeline initialized with width: {self.pipeline.width}, height: {self.pipeline.height}")
             
